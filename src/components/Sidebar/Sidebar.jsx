1baa8a5712ea0279ccb654ec21c7dac617465677
enasMetaly <enasmetwally99@gmail.com>
2024-12-08 21:20:14 +0200

    save prev promps to sidebar& integrate new chat func

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,39 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/context'
 const Sidebar = () => {
   const [extended, setExtended] = useState(false)
+  const { onSent, prevPrompts, setRecentPrompt ,newChat } = useContext(Context);
+  const loadPrompt=async (prompt) =>{
+    setRecentPrompt(prompt)
+    await onSent(prompt)
+  }
   return (
     <div className='sidebar'>
       <div className="top">
         <img onClick={()=>{setExtended(prev=>!prev)}} className='menu' src={assets.menu_icon} alt='' />
-        <div className='new-chat'>
+        <div onClick={()=>newChat()} className='new-chat'>
           <img src={assets.plus_icon} />
           {extended ? <p>New Chat</p> : null}
         </div>
         {extended
           ? <div className='recent'>
-            <p className='recent-title'>Recent</p>
-            <div className='recent-entry'>
-              <img src={assets.message_icon} />
-              <p >what is react ...</p>
+              <p className='recent-title'>Recent</p>
+              {
+                prevPrompts.map((item, index) => {
+                  return (
+                    <div onClick={()=>loadPrompt(item)} className='recent-entry' key={index}>
+                      <img src={assets.message_icon} alt="message icon" />
+                      <p>{item.slice(0,18)}...</p> 
+                    </div>
+                  )
+                })
+              }
             </div>
-          </div>
           : null
         }
-
+        
       </div>
       <div className="bottom">
         <div className='bottom-item recent-entry'>
