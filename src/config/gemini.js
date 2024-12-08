5f496bb2a7203d6a43d63b00c48c986c0f7835aa
enasMetaly <enasmetwally99@gmail.com>
2024-12-08 06:16:04 +0200

    Refactored Gemini API integration for better maintainability

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,35 +1,36 @@
-const {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } = require("@google/generative-ai");
-  
-//   const apiKey = process.env.GEMINI_API_KEY;
-const apiKey="AIzaSyDpR2kowKTaeKM3OxHAMbKjORxgydDgFjM"
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
 
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+//   const apiKey = process.env.GEMINI_API_KEY;
+const apiKey = "AIzaSyDpR2kowKTaeKM3OxHAMbKjORxgydDgFjM"
+
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function runChat(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
   });
-  
-  const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 40,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-  };
-  
-  async function run(prompt) {
-    const chatSession = model.startChat({
-      generationConfig,
-      history: [
-      ],
-    });
-  
-    const result = await chatSession.sendMessage(prompt);
-    console.log(result.response.text());
-  }
-  
-export default  run;
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text()
+}
+
+export default runChat;
